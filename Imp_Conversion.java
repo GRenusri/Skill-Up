728dbdfcfd8da8d6d72dcae4bc1fb8fc6f1e4bdf
GRenusri <69618153+GRenusri@users.noreply.github.com>
2020-08-14 20:30:32 +0530

    Add files via upload

---
+++ Imp_Conversion.java
@@ -0,0 +1,15 @@
+package pack2;
+
+public class Imp_Conversion
+{
+
+	public static void main(String[] args) 
+	{
+		byte b = 10;
+		int i = b;  // implicit type casting
+		System.out.println(" b value = "+b);
+		System.out.println(" i value = "+i);
+
+	}
+
+}
