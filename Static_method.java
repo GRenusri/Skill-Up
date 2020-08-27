d76b8077da68bc7a06c0b1444732ec6df3da0629
GRenusri <69618153+GRenusri@users.noreply.github.com>
2020-08-27 20:51:54 +0530

    Add files via upload

---
+++ Static_method.java
@@ -0,0 +1,17 @@
+package pack5;
+
+class A1
+{
+	static  int x = 100;
+	static void show()
+	{
+		System.out.println(" value = " + x);
+	}
+}
+public class Static_method 
+{
+	public static void main(String[] args) 
+	{
+	A1.show();
+	}
+}
