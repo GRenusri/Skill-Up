c3ccc44f6486ba016cc0837a4df0019df24c6f2b
GRenusri <69618153+GRenusri@users.noreply.github.com>
2020-08-28 15:32:54 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,13 @@
+package pack5;
+
+public class Autoboxing 
+{
+	public static void main(String[] args) 
+	{
+		Integer a = new Integer(3);
+		int i = a.intValue();
+		int j = a; 
+		System.out.println("a = "+a+", i = "+i+", j = "+j);	
+	}
+
+}
