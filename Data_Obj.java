85eab2375db190467368d65e37080e3080ce1ac0
GRenusri <69618153+GRenusri@users.noreply.github.com>
2020-08-28 15:27:32 +0530

    Add files via upload

---
+++ Data_Obj.java
@@ -0,0 +1,12 @@
+package pack5;
+
+public class Data_Obj 
+{
+	public static void main(String[] args) 
+	{
+	 int a =20;
+	 Integer i = Integer.valueOf(a);  
+	 Integer j = a; 
+	 System.out.println("a = "+a+" i = "+i + " j = "+j);
+	}
+}
